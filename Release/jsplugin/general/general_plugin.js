8c5689f6e4f1e60afb8b5fdc4e06842a873a2a1a
Administrateur <Administrateur@4f3009d1-116a-ef49-88a6-723a482b04ec>
2007-10-02 19:37:15 +0000

    js column creation

--- Release/jsplugin/general/general_plugin.js
+++ Release/jsplugin/general/general_plugin.js
@@ -79,32 +79,120 @@ function statusBarText(Sub) {
 
 }
 
+function getReadingSpeedColor(Sub) {
+  var len = Sub.StrippedText.length;
+  var durMS = Sub.Stop - Sub.Start;
+  var rs = (len * 1000) / (durMS - 500);
+  
+  // Pure green hue = 120° (red = 0°, blue = 240°)
+  // Base color : 0x99FF99
+
+  if (rs < 5)       return 0x9999FF;  // 240° "TOO SLOW!"
+  else if (rs < 10) return 0x99CCFF;  // 210° "Slow, acceptable.";
+  else if (rs < 13) return 0x99FFFF;  // 180° "A bit slow.";
+  else if (rs < 15) return 0x99FFCC;  // 150° "Good."
+  else if (rs < 23) return 0x99FF99;  // 120° "Perfect.";
+  else if (rs < 27) return 0xCCFF99;  //  90° "Good.";
+  else if (rs < 31) return 0xFFFF99;  //  60° "A bit fast.";
+  else if (rs < 35) return 0xFFCC99;  //  30° "Fast, acceptable.";
+  else              return 0xFF9999;  //   0° "TOO FAST!";
+}
+
+function getReadingSpeedText(Sub) {
+  var len = Sub.StrippedText.length;
+  var durMS = Sub.Stop - Sub.Start;
+  var rs = (len * 1000) / (durMS - 500);
+  var rsRounded = Math.round(rs * 10) / 10;   // rounded to 1 decimal digit  
+  return '' + rsRounded;
+}
+
 VSSPlugin = {
 
+  // Called on subtitle modifications (time or text)
   OnSubtitleModification : function(CurrentSub, PreviousSub, NextSub) {
     SetStatusBarText(statusBarText(CurrentSub));
   },
 
+  // Called when the selected subtitle change
   OnSelectedSubtitle : function(CurrentSub, PreviousSub, NextSub) {
     SetStatusBarText(statusBarText(CurrentSub));
   },
+  
+  // COLUMNS -----------------------------------------------------------------
 
-  // ----------
-
-  ExtraColumns : {
-    Col0 : {Name : 'RS', Size : 40}
+  // VSS core columns index
+  // INDEX_COL_IDX : Index of the subtitle
+  // START_COL_IDX : Start time of the subtitle
+  // STOP_COL_IDX  : Stop time of the subtitle
+  // STYLE_COL_IDX : Style of the subtitle (SSA/ASS only)
+  // TEXT_COL_IDX  : Text of the subtitle
+  
+  Initialize : function() {
+    // Extra-columns index
+    this.RS_COL_IDX = this.LAST_CORE_COL_IDX + 1; // Reading speed
   },
   
-  GetExtraColumnText : function(Index, CurrentSub) {
+  // Get the number of extra-columns (called only at VSS startup)
+  GetExtraColumnsCount : function() {
+    return 1;
+  },
+  
+  // Get the title of each extra-column (called only at VSS startup)
+  GetColumnTitle : function(Index) {
     switch(Index) {
-      case 0: return 'Col0';
+      case this.RS_COL_IDX: return 'RS';
+      default: return '';
+    }
+  },
+  
+  // Get the size of each extra-column (called only at VSS startup)
+  GetColumnSize : function(Index) {
+    switch(Index) {
+      case this.RS_COL_IDX: return 40;
+      default: return '';
     }
   },
 
-  GetExtraColumnColor : function(Index, CurrentSub) {
+  // Check if a column background can be colorized (called only at VSS startup)
+  IsColumnBGColorized : function(Index) {
     switch(Index) {
-      case 0: return 0xff0000;
+      case this.INDEX_COL_IDX: return false; 
+      case this.START_COL_IDX: return false;
+      case this.STOP_COL_IDX:  return false;
+      case this.STYLE_COL_IDX: return false;
+      case this.TEXT_COL_IDX:  return false;
+      case this.RS_COL_IDX:    return true;
+      default: return false;
+    }
+  },
+  
+  // Check if a column has custom text (called only at VSS startup)
+  HasColumnCustomText : function(Index) {
+    switch(Index) {
+      case this.INDEX_COL_IDX: return false; 
+      case this.START_COL_IDX: return false;
+      case this.STOP_COL_IDX:  return false;
+      case this.STYLE_COL_IDX: return false;
+      case this.TEXT_COL_IDX:  return false;
+      case this.RS_COL_IDX:    return true;
+      default: return false;
+    }
+  },  
+  
+  // Get the column background color (called on each cell repaint)
+  GetColumnBGColor : function(Index, CurrentSub, PreviousSub, NextSub) {
+    switch(Index) {
+      case this.RS_COL_IDX: return getReadingSpeedColor(CurrentSub);
+      default: return 0xffffff;
+    }
+  },
+  
+  // Get the text of the extra-column (called on each cell repaint)
+  GetColumnText : function(Index, CurrentSub, PreviousSub, NextSub) {
+    switch(Index) {
+      case this.RS_COL_IDX: return getReadingSpeedText(CurrentSub);
+      default: '';
     }
   }
-
+    
 }
