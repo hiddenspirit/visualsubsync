39ab949ba77c2fe4bbdb81301e300ba9258ac58b
Administrateur <Administrateur@4f3009d1-116a-ef49-88a6-723a482b04ec>
2007-12-22 15:30:19 +0000

    some work on tag conversion

--- dunit/MiscToolsUnitTests.pas
+++ dunit/MiscToolsUnitTests.pas
@@ -17,6 +17,8 @@ type
     // Test methods
     procedure TestWideStringFind;
 
+    procedure TestConvertSSAToSRT;
+    procedure TestConvertSRTToSSA;    
   end;
 
 implementation
@@ -36,6 +38,20 @@ begin
   CheckEquals(11, WideStringFind(1, 'aBcD eFgh iJk','iJk', False, True), 'Whole word end of string');
   CheckEquals(6, WideStringFind(1, 'aBcD eFgh iJk','eFgh', False, True), 'Whole word middle of string');
   CheckEquals(11, WideStringFind(2, 'aBcD eFgh aBcD','aBcD', False, True), 'Offset > 1');
+end;
+
+procedure TMiscToolsTests.TestConvertSSAToSRT;
+begin
+  CheckEquals('<i>italic</i>', ConvertSSAToSRT('{\i1}italic{\i0}'), 'italic');
+  CheckEquals('<b>bold</b>', ConvertSSAToSRT('{\b1}bold{\b0}'), 'bold');
+  CheckEquals('<u>underlined</u>', ConvertSSAToSRT('{\u1}underlined{\u0}'), 'underlined');
+end;
+
+procedure TMiscToolsTests.TestConvertSRTToSSA;
+begin
+  CheckEquals('{\i1}italic{\i0}', ConvertSRTToSSA('<i>italic</i>'), 'italic');
+  CheckEquals('{\b1}bold{\b0}', ConvertSRTToSSA('<b>bold</b>'), 'bold');
+  CheckEquals('{\u1}underlined{\u0}', ConvertSRTToSSA('<u>underlined</u>'), 'underlined');
 end;
 
 initialization
