02f8290b51d0f57b49a83519e6a45dc27a0b660d
Administrateur <Administrateur@4f3009d1-116a-ef49-88a6-723a482b04ec>
2008-04-26 19:30:00 +0000

    copy subtitle / load presets

---
+++ VSSClipboardUnit.pas
@@ -0,0 +1,118 @@
+unit VSSClipboardUnit;
+
+interface
+
+uses VirtualTrees;
+
+procedure CopyVTVToClipboard(vtv: TVirtualStringTree);
+procedure PasteClipboardToVTV(vtv: TVirtualStringTree);
+  
+var
+  VSSClipBoardFORMAT : Cardinal;
+
+const
+  CopyPasteStreamVersion = 1;
+
+implementation
+
+uses Windows, SubStructUnit, Classes, SysUtils, MiscToolsUnit, TntClipBrd, TntSysUtils;
+
+//==================================================================================================
+
+procedure CopyVTVToClipboard(vtv: TVirtualStringTree);
+var Node : PVirtualNode;
+    NodeData : PTreeData;
+    Msg : WideString;
+    SubtitleRange : TSubtitleRange;
+    MemStream : TMemoryStream;
+    MemHandle : THandle;
+    MemPointer : Pointer;
+begin
+  if (vtv.SelectedCount <= 0) then
+    Exit;
+
+  // TODO : http://delphi.about.com/od/windowsshellapi/a/clipboard_spy_3.htm
+  // http://www.delphipages.com/news/detaildocs.cfm?ID=145
+  // http://homepage2.nifty.com/Mr_XRAY/Halbow/Notes/N014.html
+  // TACtion HandlesTarget
+
+  MemStream := TMemoryStream.Create;
+  SaveToStreamInt(MemStream, CopyPasteStreamVersion);
+  SaveToStreamInt(MemStream, vtv.SelectedCount);
+  Node := vtv.GetFirstSelected;
+  while Assigned(Node) do
+  begin
+    NodeData := vtv.GetNodeData(Node);
+    SubtitleRange := NodeData.Range;
+    Msg := Msg + Sub2SrtString(SubtitleRange) + CRLF + CRLF;
+    SubtitleRange.SaveToStream(MemStream);
+    Node := vtv.GetNextSelected(Node);
+  end;
+  if (Length(Msg) > 0) then
+  begin
+    TntClipboard.Open;
+    try
+      TntClipboard.Clear;
+      // Set text format data
+      TntClipboard.AsWideText := Trim(Msg);
+      // Set custom format data
+      MemHandle := GlobalAlloc(GMEM_DDESHARE or GMEM_MOVEABLE, MemStream.Size);
+      MemPointer := GlobalLock(MemHandle);
+      MemStream.Position := 0;
+      MemStream.ReadBuffer(MemPointer^, MemStream.Size);
+      TntClipboard.SetAsHandle(VSSClipBoardFORMAT, MemHandle);
+      GlobalUnLock(MemHandle);
+    finally
+      TntClipboard.Close;
+    end;
+  end;
+  MemStream.Free;
+end;
+
+// -------------------------------------------------------------------------------------------------
+
+procedure PasteClipboardToVTV(vtv: TVirtualStringTree);
+var MemHandle : THandle;
+    MemPointer : Pointer;
+    MemStream : TMemoryStream;
+
+    StreamVersion, SubCount, i : Integer;
+    SubRange : TSubtitleRange;
+begin
+  if TntClipboard.HasFormat(VSSClipBoardFORMAT) then
+  begin
+    MemStream := TMemoryStream.Create;
+    TntClipboard.Open;
+    MemHandle := TntClipboard.GetAsHandle(VSSClipBoardFORMAT);
+    MemPointer := GlobalLock(MemHandle);
+    MemStream.WriteBuffer(MemPointer^, GlobalSize(MemHandle));
+    GlobalUnlock(MemHandle);
+    TntClipboard.Close;
+
+    MemStream.Position := 0;
+    LoadFromStreamInt(MemStream, StreamVersion);
+    if (StreamVersion = CopyPasteStreamVersion) then
+    begin
+      // Load each sub
+      LoadFromStreamInt(MemStream, SubCount);
+      SubRange := TSubtitleRange.Create;
+      for i := 0 to SubCount-1 do
+      begin
+        SubRange.LoadFromStream(MemStream);
+        // TODO : really add subtitle / sort / undo
+        //AddSubtitle(SubRange);
+      end;
+      SubRange.Free;
+    end;
+    MemStream.Free;
+  end;
+end;
+
+//==================================================================================================
+
+initialization
+  VSSClipBoardFORMAT := RegisterClipboardFormat(PChar('CF_VisualSubSync'));
+
+//==================================================================================================
+end.
+//==================================================================================================
