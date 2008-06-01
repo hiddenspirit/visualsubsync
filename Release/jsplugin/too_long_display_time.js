602c1698eb8744022908a20fcb882485b595f73d
Administrateur <Administrateur@4f3009d1-116a-ef49-88a6-723a482b04ec>
2008-06-01 18:52:39 +0000

    time and rs support

--- Release/jsplugin/too_long_display_time.js
+++ Release/jsplugin/too_long_display_time.js
@@ -1,6 +1,8 @@
 // Too long display time
 // (christophe.paris <at> free.fr)
 
+LoadScript("common/tools.js");
+
 VSSPlugin = {
   // ----- Plugin constant -----
   Name : 'Too long display time',
@@ -10,7 +12,9 @@ VSSPlugin = {
   Message : 'Subtitle display time is too long :',
 
   // ----- Plugin parameters available from VSS GUI (name must start with "Param") -----
-  ParamMinCPS : { Value : 4, Unit : 'Char/s' },
+  ParamMinCPS : { Value : 4, Unit : 'Char/s', Description : 'Check for subtitle with Char/s < to this (0 to disable)' },
+  ParamMinTime : { Value : 10, Unit : 's', Description : 'Check for subtitle with time > to this (0 to disable)' },
+  ParamMinRS : { Value : 5, Unit : '' , Description : 'Check for subtitle with reading speed < to this (0 to disable)' },
   ParamIgnoreLinesOf : { Value : 0, Unit : 'Characters' },
 
   // ----- HasError method called for each subtitle during the error checking -----
@@ -19,17 +23,29 @@ VSSPlugin = {
   // Don't forget that PreviousSub and NextSub can be null
   HasError : function(CurrentSub, PreviousSub, NextSub) {
     // Ignore lines with length < to this.ParamIgnoreLinesOf.Value
-    if(CurrentSub.StrippedText.length <= this.ParamIgnoreLinesOf.Value)
-    {
+    var len = CurrentSub.StrippedText.length;
+    if(len <= this.ParamIgnoreLinesOf.Value) {
         return '';
     }
     
-    Duration = CurrentSub.Stop - CurrentSub.Start;
-    CharPerSec = (CurrentSub.StrippedText.length * 1000) / Duration;
-    if (CharPerSec < this.ParamMinCPS.Value) {
-    	return (Math.round(CharPerSec) + ' ' + this.ParamMinCPS.Unit);
-    } else {
-    	return '';
+    // Check for duration
+    var durSec = (CurrentSub.Stop - CurrentSub.Start) / 1000;
+    if (this.ParamMinTime.Value > 0 && durSec > this.ParamMinTime.Value) {
+      return decimal1Round(durSec) + ' ' + this.ParamMinTime.Unit;
     }
+    
+    // Check for characters per second
+    var charPerSec = len / durSec;
+    if (charPerSec < this.ParamMinCPS.Value) {
+      return decimal1Round(charPerSec) + ' ' + this.ParamMinCPS.Unit;
+    }
+    
+    // Check for reading speed
+    var rs = len / (durSec - 0.5);
+    if (rs < this.ParamMinRS.Value) {
+      return 'rs: ' + decimal1Round(rs);
+    }
+    
+    return '';
   }
 };
--- Release/jsplugin/too_short_display_time.js
+++ Release/jsplugin/too_short_display_time.js
@@ -1,6 +1,8 @@
 // Too short display time
 // (christophe.paris <at> free.fr)
 
+LoadScript("common/tools.js");
+
 VSSPlugin = {
   // ----- Plugin constant -----
   Name : 'Too short display time',
@@ -10,19 +12,34 @@ VSSPlugin = {
   Message : 'Subtitle display time is too short :',
 
   // ----- Plugin parameters available from VSS GUI (name must start with "Param") -----
-  ParamMaxCPS : { Value : 25, Unit : 'Char/s' },
+  ParamMaxCPS : { Value : 25, Unit : 'Char/s', Description : 'Check for subtitle with Char/s > to this (0 to disable)' },
+  ParamMaxRS : { Value : 35, Unit : '' , Description : 'Check for subtitle with reading speed > to this (0 to disable)' },
 
   // ----- HasError method called for each subtitle during the error checking -----
   // If there is an error on CurrentSub return a string containing the error description.
   // Otherwise return an empty string.
   // Don't forget that PreviousSub and NextSub can be null
   HasError : function(CurrentSub, PreviousSub, NextSub) {
-    Duration = CurrentSub.Stop - CurrentSub.Start;
-    CharPerSec = (CurrentSub.StrippedText.length * 1000) / Duration;
-    if (CharPerSec > this.ParamMaxCPS.Value) {
-    	return (Math.round(CharPerSec) + ' ' + this.ParamMaxCPS.Unit);
-    } else {
-    	return '';
+    // Check for duration
+    var durMS = CurrentSub.Stop - CurrentSub.Start;
+    if (durMS < VSSCore.MinimumDuration) {
+      return decimal1Round(durMS) + 'ms';
     }
+    
+    // Check for characters per second
+    var len = CurrentSub.StrippedText.length;
+    var durSec = durMS / 1000;
+    var charPerSec = len / durSec;
+    if (charPerSec > 0 && charPerSec > this.ParamMaxCPS.Value) {
+      return decimal1Round(charPerSec) + ' ' + this.ParamMaxCPS.Unit;
+    }
+    
+    // Check for reading speed
+    var rs = len / (durSec - 0.5);
+    if (rs > 0 && rs > this.ParamMaxRS.Value) {
+      return 'rs: ' + decimal1Round(rs);
+    }
+    
+    return '';
   }
 };
