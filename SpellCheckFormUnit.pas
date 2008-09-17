48f049bd229fffae70df489b2c89bdc71ae95f09
Administrateur <Administrateur@4f3009d1-116a-ef49-88a6-723a482b04ec>
2008-09-17 17:56:37 +0000

    add spell check form (empty for now)

---
+++ SpellCheckFormUnit.pas
@@ -0,0 +1,24 @@
+unit SpellCheckFormUnit;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  TSpellCheckForm = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  SpellCheckForm: TSpellCheckForm;
+
+implementation
+
+{$R *.dfm}
+
+end.
