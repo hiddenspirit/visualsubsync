2ac652514366f3846d1704f5367a3778bf3d0bc6
toffparis <toffparis@4f3009d1-116a-ef49-88a6-723a482b04ec>
2008-11-23 10:09:47 +0000

    add *.mov to video file filter

--- ProjectUnit.pas
+++ ProjectUnit.pas
@@ -285,7 +285,7 @@ begin
   TntOpenDialog1.FileName := EditVideoFilename.Text;
   TntOpenDialog1.Filter :=
     'Video files|*.AVI;*.OGM;*.MKV;*.MKA;*.MP4;*.DIVX;*.RM;' +
-    '*.RMVB;*.MPEG;*.MPG;*.VOB;*.AVS;*.WMV' + '|' +
+    '*.RMVB;*.MPEG;*.MPG;*.VOB;*.AVS;*.WMV;*.MOV' + '|' +
     'All files (*.*)|*.*';
   if TntOpenDialog1.Execute then
   begin
