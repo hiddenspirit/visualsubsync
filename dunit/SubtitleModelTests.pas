da8adab41922bedc4dffe490c12454f323d4d92c
toffparis <toffparis@4f3009d1-116a-ef49-88a6-723a482b04ec>
2009-01-03 15:37:29 +0000

    some work on a new subtitle model

---
+++ dunit/SubtitleModelTests.pas
@@ -0,0 +1,209 @@
+unit SubtitleModelTests;
+
+interface
+
+uses
+  SubtitleModel,
+  TestFrameWork;
+
+type
+  TSubtitleModelTests = class(TTestCase)
+  private
+    FSubtitleModel : TSubtitleModel;
+
+  protected
+    procedure SetUp; override;
+    procedure TearDown; override;
+
+  published
+
+    // Test methods
+    procedure TestCreateSubtitle;
+    procedure TestGet;
+    procedure TestSetStart;
+    procedure TestSetStop;
+    procedure TestSetText;
+    procedure TestDeleteSubtitle;
+
+  end;
+
+implementation
+
+uses Classes, SysUtils;
+
+procedure TSubtitleModelTests.SetUp;
+begin
+  FSubtitleModel := TSubtitleModel.Create;
+end;
+
+procedure TSubtitleModelTests.TearDown;
+begin
+  FreeAndNil(FSubtitleModel);
+end;
+
+procedure TSubtitleModelTests.TestCreateSubtitle;
+var Sub : TSubtitleItem;
+    Transaction : TCompositeSubTask;
+    SubId : Integer;
+begin
+  CheckEquals(0, FSubtitleModel.GetCount, 'No subtitle yet.');
+  FSubtitleModel.BeginTransaction;
+  try
+    Sub := FSubtitleModel.CreateSubtitle;
+  finally
+    Transaction := FSubtitleModel.EndTransaction;
+  end;
+  CheckNotNull(Sub, 'Subtitle exists.');
+  CheckEquals(1, FSubtitleModel.GetCount, '1 subtitle.');
+  
+  SubId := Sub.GetId;
+  Transaction.UndoTask;
+  CheckEquals(0, FSubtitleModel.GetCount, '0 subtitle.');
+  Sub := FSubtitleModel.Get(SubId);
+  CheckNull(Sub, 'No subtitle anymore.');
+
+  Transaction.DoTask;
+  CheckEquals(1, FSubtitleModel.GetCount, '1 subtitle again.');
+  Sub := FSubtitleModel.Get(SubId);
+  CheckNotNull(Sub, 'Subtitle exists again.');
+  CheckEquals(SubId, Sub.GetId, 'Same id.');
+    
+  FreeAndNil(Transaction);
+end;
+
+procedure TSubtitleModelTests.TestGet;
+var Sub, Sub2 : TSubtitleItem;
+    Transaction : TCompositeSubTask;
+begin
+  Sub := FSubtitleModel.Get(100);
+  CheckNull(Sub, 'Subtitle doesnt exists yet.');
+  FSubtitleModel.BeginTransaction;
+  try
+    Sub := FSubtitleModel.CreateSubtitle;
+  finally
+    Transaction := FSubtitleModel.EndTransaction;
+  end;
+  FreeAndNil(Transaction);  
+  CheckNotNull(Sub, 'Subtitle created.');
+  Sub2 := FSubtitleModel.Get(Sub.GetId);
+  CheckNotNull(Sub2, 'Subtitle not null.');
+  CheckEquals(Sub.GetId, Sub2.GetId, 'Subtitle is same.');
+end;
+
+procedure TSubtitleModelTests.TestSetStart;
+var Sub : TSubtitleItem;
+    Transaction : TCompositeSubTask;
+begin
+  FSubtitleModel.BeginTransaction;
+  try
+    Sub := FSubtitleModel.CreateSubtitle;
+  finally
+    Transaction := FSubtitleModel.EndTransaction;
+  end;
+  FreeAndNil(Transaction);
+  
+  FSubtitleModel.BeginTransaction;
+  try
+    FSubtitleModel.SetSubtitleStart(Sub, 10);
+  finally
+    Transaction := FSubtitleModel.EndTransaction;
+  end;
+  
+  CheckEquals(10, Sub.GetStart, 'Start = 10.');
+  Transaction.UndoTask;
+  CheckEquals(0, Sub.GetStart, 'Start = 0.');
+
+  FreeAndNil(Transaction);
+end;
+
+procedure TSubtitleModelTests.TestSetStop;
+var Sub : TSubtitleItem;
+    Transaction : TCompositeSubTask;
+begin
+  FSubtitleModel.BeginTransaction;
+  try
+    Sub := FSubtitleModel.CreateSubtitle;
+  finally
+    Transaction := FSubtitleModel.EndTransaction;
+  end;
+  FreeAndNil(Transaction);
+  
+  FSubtitleModel.BeginTransaction;
+  try
+    FSubtitleModel.SetSubtitleStop(Sub, 10);
+  finally
+    Transaction := FSubtitleModel.EndTransaction;
+  end;
+  
+  CheckEquals(10, Sub.GetStop, 'Stop = 10.');
+  Transaction.UndoTask;
+  CheckEquals(0, Sub.GetStop, 'Stop = 0.');
+
+  FreeAndNil(Transaction);
+end;
+
+procedure TSubtitleModelTests.TestSetText;
+var Sub : TSubtitleItem;
+    Transaction : TCompositeSubTask;
+begin
+  FSubtitleModel.BeginTransaction;
+  try
+    Sub := FSubtitleModel.CreateSubtitle;
+  finally
+    Transaction := FSubtitleModel.EndTransaction;
+  end;
+  FreeAndNil(Transaction);
+  
+  FSubtitleModel.BeginTransaction;
+  try
+    FSubtitleModel.SetSubtitleText(Sub, '1234 text');
+  finally
+    Transaction := FSubtitleModel.EndTransaction;
+  end;
+  
+  CheckEquals('1234 text', Sub.GetText, 'Stop = 1234 text.');
+  Transaction.UndoTask;
+  CheckEquals('', Sub.GetText, 'Stop = .');
+
+  FreeAndNil(Transaction);
+end;
+
+procedure TSubtitleModelTests.TestDeleteSubtitle;
+var Sub : TSubtitleItem;
+    Transaction : TCompositeSubTask;
+    SubId : Integer;
+begin
+  CheckEquals(0, FSubtitleModel.GetCount, 'No subtitle yet.');
+  FSubtitleModel.BeginTransaction;
+  try
+    Sub := FSubtitleModel.CreateSubtitle;
+  finally
+    Transaction := FSubtitleModel.EndTransaction;
+  end;
+  FreeAndNil(Transaction);
+  CheckNotNull(Sub, 'Subtitle exists.');
+  CheckEquals(1, FSubtitleModel.GetCount, 'One subtitle.');
+
+  SubId := Sub.GetId;
+  FSubtitleModel.BeginTransaction;
+  try
+    FSubtitleModel.DeleteSubtitle(Sub);
+  finally
+    Transaction := FSubtitleModel.EndTransaction;
+  end;
+  CheckEquals(0, FSubtitleModel.GetCount, 'No subtitle.');
+
+  Transaction.UndoTask;
+
+  CheckEquals(1, FSubtitleModel.GetCount, 'One subtitle again.');
+  Sub := FSubtitleModel.Get(SubId);
+  CheckNotNull(Sub, 'Subtitle exists again.');
+  CheckEquals(SubId, Sub.GetId, 'Same id.');
+end;
+
+initialization
+  TestFramework.RegisterTest('SubtitleModelTests Suite',
+    TSubtitleModelTests.Suite);
+
+end.
+ 
