7297925788571e0b93b102cdc9207b79aacaac32
spirit <hiddenspirit@gmail.com>
2013-01-21 09:04:38 +0100

    SceneChangeFile: fix contains().

--- vss-companion/common.py
+++ vss-companion/common.py
@@ -12,7 +12,7 @@ from util.media_hash import MediaHash
 
 
 FFINDEX_MAX_FILES = 24
-BUILD_DATE = "2013-01-21 07:16:55"
+BUILD_DATE = "2013-01-21 08:03:53"
 
 APP_NAME = "VisualSubSync-Companion"
 APP_DATA_DIR = os.path.join(get_app_data_dir(), APP_NAME)
