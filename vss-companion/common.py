ed9bcbc77a47f2338bf40448a6c0882b363aab49
spirit <spirit@localhost>
2014-01-22 15:44:59 +0100

    Build date

--- vss-companion/common.py
+++ vss-companion/common.py
@@ -14,7 +14,7 @@ from util.media_hash import MediaHash
 
 VERBOSE = False
 FFINDEX_MAX_FILES = 24
-BUILD_DATE = "2013-11-28 02:30:40"
+BUILD_DATE = "2014-01-22 14:41:47"
 
 APP_NAME = "VisualSubSync-Companion"
 APP_DATA_DIR = os.path.join(get_app_data_dir(), APP_NAME)
