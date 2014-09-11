3813c9b17c5fff513055e1603a36d12793f3d7b0
spirit <79175+hiddenspirit@users.noreply.github.com>
2014-09-11 19:10:59 +0200

    Add .m4v video type.

--- vss-companion/common.py
+++ vss-companion/common.py
@@ -14,7 +14,7 @@ from util.media_hash import MediaHash
 
 VERBOSE = False
 FFINDEX_MAX_FILES = 32
-BUILD_DATE = "2014-08-17 14:16:29"
+BUILD_DATE = "2014-09-04 15:30:48"
 
 APP_NAME = "VisualSubSync-Companion"
 APP_DATA_DIR = os.path.join(get_app_data_dir(), APP_NAME)
@@ -24,6 +24,7 @@ VIDEO_EXTS = [
     ".flv",
     ".mkv",
     ".mp4",
+    ".m4v",
     ".mpg",
     ".ogg",
     ".ogm",
