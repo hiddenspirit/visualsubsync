ba00ec14f2ea1069e6d074013cd909aab6bc13a3
spirit <spirit@localhost>
2014-09-11 19:11:30 +0200

    Typo

--- vss-companion/util/detect_encoding.py
+++ vss-companion/util/detect_encoding.py
@@ -8,13 +8,13 @@ FAILSAFE_ENCODING = "cp1252"
 def detect_encoding(buf, default=None):
     if buf.startswith(codecs.BOM_UTF8):
         encoding = "utf-8-sig"
+    elif (buf.startswith(codecs.BOM_UTF16_LE) or
+          buf.startswith(codecs.BOM_UTF16_BE)):
+        encoding = "utf-16"
     elif (buf.startswith(codecs.BOM_UTF32_LE) or
           buf.startswith(codecs.BOM_UTF32_BE)):
         encoding = "utf-32"
-    elif (buf.startswith(codecs.BOM_UTF16_LE) or
-          buf.startswith(codecs.BOM_UTF16_LE)):
-        encoding = "utf-16"
     else:
-        encoding = (default or locale.getpreferredencoding() or
+        encoding = (default or locale.getpreferredencoding(False) or
                     FAILSAFE_ENCODING)
     return encoding
